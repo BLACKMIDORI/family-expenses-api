0b5da5d55a44a3f54ff341e8e08eeebd2e321b34
Railson Ferreira <railson.info@gmail.com>
2024-01-15 00:57:04 +0000

    chore: Remove unused code

--- src/main.go
+++ src/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"crypto/sha512"
-	"encoding/hex"
-	"fmt"
 	// This is required to initialize controllers
 	_ "family-expenses-api/controllers"
 	"github.com/a-h/awsapigatewayv2handler"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/google/uuid"
 	"log"
 	"net/http"
 	"os"
@@ -28,14 +24,4 @@ func main() {
 
 func isRunningOnLambda() bool {
 	return os.Getenv("AWS_LAMBDA_RUNTIME_API") != ""
-}
-
-func teste() {
-	refreshToken := ""
-	for i := 0; i < 10; i++ {
-		sha_512 := sha512.New()
-		sha_512.Write([]byte(uuid.New().String()))
-		refreshToken += hex.EncodeToString(sha_512.Sum(nil))
-	}
-	fmt.Println(len(refreshToken), refreshToken)
 }
